ad63eb613b0d01a475019e72f2c7877fdb34baa7
ruyanzhang <739406122@qq.com>
2019-01-22 14:52:34 +0800

    Delete app.js

--- src/services/app.js
+++
@@ -1,9 +0,0 @@
-import { request } from '../utils'
-import * as serverConfig from '../utils/serverConfig'
-
-export async function login (params) {
-  var url = serverConfig.LOGINURL +'login?username='+params.username + '&password=' + params.password;
-  // return request(url, {
-  //   method: 'post',
-  // });
-}
